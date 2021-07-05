5e66f35ac85ecc4810c6fa13566c3c98b20e2739
safuvanpp <safuvanpp1993@gmail.com>
2021-07-05 12:19:29 +0530

    Update findmissingnumber_m1.py

--- findmissingnumber_m1.py
+++ findmissingnumber_m1.py
@@ -5,6 +5,11 @@ Created on Mon Jul  5 10:52:13 2021
 @author: safuv
 """
 
+"""
+You are given a list of n-1 integers and these integers are in the range of 1 to n. There are no duplicates in the list. 
+One of the integers is missing in the list. Write an efficient code to find the missing integer.
+"""
+
 #method 1
 
 """
@@ -12,7 +17,7 @@ missing number =  n*(n+1)/2 - (sum of the list)
 """
 
 def findmissingnumber(arr,n):
-    total_sum = (n+1)*(n+2)/2
+    total_sum = int((n+1)*(n+2)/2)
     print(total_sum)
     for i in array:
         total_sum -= i
